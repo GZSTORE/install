75ae958d083677a49755b7ed827371992d3fbfbc
Fahri-Hosting <147958381+GZSTORE@users.noreply.github.com>
2025-06-24 03:06:23 +0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "ISI DATA VPS ANDA"
 echo "SILAHKAN ISI DATA VPS ANDA"
-read -p "Masukan User Vps Kamu... " user
-read -p "Masukan IP Vps Kamu... " ip
+read -p "Masukan User Vps Kamu... ," user
+read -p "Masukan IP Vps Kamu... ," ip
 
 #informasi
 echo "Sedang Melakukan Login Ke Vps Anda..."
