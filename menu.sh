20a99ea42b5d79bf86d023cde668775cc3ebb301
Fahri-Hosting <147958381+GZSTORE@users.noreply.github.com>
2025-06-24 02:54:00 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "ANDA TELAH MEMBUKA MENU.SH, BERIKUT LIST MENU NYA..."
+echo "bash install.sh" #Untuk Install PKG
+echo "bash ip.sh" #Untuk Login Vps
+echo "bash menu.sh" #untuk melihat list menu
+
+echo "Untuk List Menu Lainnya Tunggu Kami Update!!"
