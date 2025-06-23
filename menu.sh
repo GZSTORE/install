92456ad3f7260764e9fbd18c249f3b39a95ebb72
Fahri-Hosting <147958381+GZSTORE@users.noreply.github.com>
2025-06-24 03:05:21 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,6 +3,5 @@ echo "TELAH MEMBUKA MENU.SH, BERIKUT LIST MENU NYA..."
 echo "bash install.sh | #Untuk Install PKG"
 echo "bash login.sh | #Untuk Login Vps"
 echo "bash menu.sh | #untuk melihat list menu"
-
-#lala
+echo ""
 echo "Untuk List Menu Lainnya Tunggu Kami Update!!"
